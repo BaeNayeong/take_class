83d316318d651f033200d2b663a057f215c3affe
nyeong1 <55578809+nyeong1@users.noreply.github.com>
2019-11-26 23:43:26 +0900

    1126 commit
    변경 사항 : mini world에서 수행하는 일들을 나타내던 기존의 University 클래스를 Person, Student, Professor 클래스로 나누었다. 
    Person : Scanner 객체 생성 / DB와의 connection / Student와 Professor의 부모 클래스
    Student : mini world에서 학생이 수행하는 일 수행
    Professor : mini world에서 교수가 수행하는 일 수행 구현 예정

--- TermProject/src/Main.java
+++ TermProject/src/Main.java
@@ -1,27 +1,31 @@
+/*
+ * <데이터베이스 시스템 term project>
+ * 작성자 : 배나영
+ * 학번 : 2015023025
+ * 학과 : 소프트웨어학과
+ * Mini world : 학교 수강 시스템
+*/
+
 import java.sql.*;
 import java.util.Scanner;
 
-class University{
+/*
+ * Person class
+ * 기능과 목적 : 
+ * mysql과 connect하는 기능, Scanner 생성
+ * Student와 Professor가 상속하도록 하여 두 클래스에서 별도로 mysql에 연결해줄 필요가 없도록 한다.  
+*/
+class Person{
+	
+	Scanner scan=new Scanner(System.in);
 	
 	Connection con = null;
     Statement stmt = null;
     PreparedStatement pstmt = null;
     ResultSet rs = null;
     
-	private int number; //수업을 수강할 때 로그인할 학번
-	private String email; //수업을 수강할 때 로그인할 이메일	
-	
-	// 생성자에서  MySQL 연결
-	public University() { 
-		
-		try{
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			con=DriverManager.getConnection("jdbc:mysql://192.168.56.101:4567/university","yeong","1234");
-			stmt=con.createStatement();
-			
-		}catch(Exception e){ System.out.println(e);}
-		
-	}
+	protected int number; // 학번 또는 교수번호
+	protected String email; // 로그인 할 때 사용할 이메일 	
 	
 	public void setNumber(int number) {
 		this.number=number;
@@ -39,19 +43,106 @@ class University{
 		return email;
 	}
 	
+	// 사용자가 메뉴를 선택할 수 있도록 하는 메소드 
+	public int getChoice() { 
+		int choice=scan.nextInt();
+		return choice;
+	}
+    
+    // 생성자에서 mysql connection
+    public Person() {
+    	try{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con=DriverManager.getConnection("jdbc:mysql://192.168.56.101:4567/university","yeong","1234");
+			stmt=con.createStatement();
+			
+		}catch(Exception e){ System.out.println(e);}
+    }
+    
+    // ResultSet, PreparedStatement, Statement, Connection, Scanner 닫아줌
+    public void closeAll() {
+		try {
+			if(rs!=null) rs.close();
+			if(stmt!=null) stmt.close();
+			if(pstmt!=null) pstmt.close();
+			if(con!=null) con.close();
+			
+		} catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+    
+    // 시작화면
+    // 학생으로 로그인할 것인지 교수로 로그인할 것인지 결정한다
+    public int startMenu() {
+    	
+    	int perNum = 0; // 누구로 로그인할 것인지 결정하는 값을 받아주는 변수
+    	
+    	System.out.println("===========================================");
+    	System.out.println("로그인 화면");
+    	System.out.println("학생으로 로그인할 것인지 교수로 로그인할 것인지 고르시오");
+    	System.out.println("(1. 학생 로그인 / 2. 교수 로그인)");
+    	System.out.println("===========================================");
+    	
+    	perNum=scan.nextInt();
+    	
+    	return perNum;
+    }
+}
+
+/*
+ * Student class (학생 클래스)
+ * mini world에서 학생에 해당한다.
+ * 학번과 이메일로 로그인
+ * 전체 수업 목록 확인, 자신이 듣는 과목 확인, 수강 취소, 특정한 교수가 담당하는 과목 확인을 수행할 수 있다.
+ */
+class Student extends Person{
+	
+	// 생성자에서  MySQL 연결
+	public Student() { 
+		super();
+	}
+	
 	// 학생의 학번과 메일로 로그인하는 메소드 
-	public boolean loginStudent(int number, String mail) {
+	public boolean loginStudent() {
+		
+		System.out.println("학번 : ");
+		int inputNum = scan.nextInt();
+		System.out.println("이메일 : ");
+		scan.nextLine();
+		String inputMail = scan.nextLine();
+		
 		try {
 			pstmt=con.prepareStatement("SELECT * FROM STUDENT WHERE Sno=? and mail=?");
-			pstmt.setInt(1, getNumber());
-			pstmt.setString(2, getMail());
+			pstmt.setInt(1, inputNum);
+			pstmt.setString(2, inputMail);
+					
 			
 		}catch(Exception e) {
 			e.printStackTrace();
 			return false;
 		}
-		System.out.println("<<로그인 성공!>>");
+		
+		System.out.println("<<로그인 성공!>>");		
+		
+		// 로그인에 성공했으므로 학번/교수번호와 이메일 변수에 저장해준다
+		setNumber(inputNum);
+		setMail(inputMail);
+
 		return true;
+	}
+	
+	// 로그인 다음 보여지는 화면
+	// 시스템에서 학생의 동작 수행
+	public void showMain() {
+		System.out.println("==========================================");
+		System.out.println("1. 수업 목록 보기");
+		System.out.println("2. 내 수강 리스트 출력하기");
+		System.out.println("3. 수강하기");
+		System.out.println("4. 수강 취소하기");
+		System.out.println("5. 교수 담당 과목 확인");
+		System.out.println("0. 종료");
+		System.out.println("==========================================");
 	}
 	
 	// 전체 수업 리스트를 확인하는 메소드 
@@ -85,7 +176,6 @@ class University{
 			while(rs.next()) {
 				System.out.println(rs.getInt(1)+"\t"+rs.getString(2)+"\t"+rs.getInt(3)+"\t"+rs.getString(4));
 			}
-
 			
 		}catch(Exception e) {
 			e.printStackTrace();
@@ -94,12 +184,19 @@ class University{
 	}
 	
 	// 학생이 수업을 신청할 수 있도록 하는 메소드 
-	public void registerClass(int classNumber) {
+	public void registerClass() {
+		
+		System.out.println("수강하고 싶은 과목 번호를 입력하시오."); 
+		scan.nextLine();	
+		
+		int subject=scan.nextInt(); // 과목 번호
+		
 		try {
+			
 			pstmt=con.prepareStatement("INSERT INTO TAKE_CLASS VALUES(?, ?)");
 			
 			pstmt.setInt(1, getNumber());
-			pstmt.setInt(2, classNumber);
+			pstmt.setInt(2, subject);
 			pstmt.executeUpdate();
 			
 		}catch(SQLException e) {
@@ -112,13 +209,16 @@ class University{
 	}
 	
 	// 학생이 수업을 취소할 수 있도록 하는 메소드
-	public void cancelClass(String className) {
+	public void cancelClass() {
+		
+		System.out.println("수강 취소하고 싶은 과목 이름을 입력하시오"); scan.nextLine();
+		String subject=scan.nextLine();
+		
 		try {
-			//(SELECT Cnum FROM CLASS WHERE Cname=?)
 			
 			pstmt=con.prepareStatement("DELETE FROM TAKE_CLASS WHERE Snum=? AND Cnum=?");
 			pstmt.setInt(1, number);
-			pstmt.setString(2, className);
+			pstmt.setString(2, subject);
 			pstmt.executeUpdate();
 			
 		}catch(Exception e) {
@@ -127,10 +227,17 @@ class University{
 	}
 	
 	// 검색한 교수가 맡은 수업을 출력하는 메소드
-	public void professorClass(String professor) {
+	/*
+	 * 에러났음
+	 */
+	public void professorClass() {
+		
+		System.out.println("교수 담당 과목 검색"); scan.nextLine();
+		String professor=scan.nextLine();
+		
 		try {
 			
-			pstmt=con.prepareStatement("SELECT P.Pname C.Cname FROM PROFESSOR P, CLASS C WHERE P.Pno=C.Pnum AND P.Pname=?");
+			pstmt=con.prepareStatement("SELECT P.Pname C.Cname FROM PROFESSOR AS P, CLASS AS C WHERE P.Pno=C.Pnum AND P.Pname=?");
 			pstmt.setString(1, professor);
 			rs = pstmt.executeQuery();
 			
@@ -147,27 +254,16 @@ class University{
 		}
 	}
 	
-	public void showMain() {
-		System.out.println("==========================================");
-		System.out.println("1. 수업 목록 보기");
-		System.out.println("2. 내 수강 리스트 출력하기");
-		System.out.println("3. 수강하기");
-		System.out.println("4. 수강 취소하기");
-		System.out.println("5. 교수 담당 과목 확인");
-		System.out.println("0. 종료");
-		System.out.println("==========================================");
-	}
+}
+/*
+ * Professor class (교수 클래스)
+ * mini world에서 교수에 해당한다.
+ * 교수번호과 이메일로 로그인
+ */
+class Professor extends Person{
 	
-	
-	public void closeDB() {
-		try {
-			if(rs!=null) rs.close();
-			if(stmt!=null) stmt.close();
-			if(pstmt!=null) pstmt.close();
-			
-		} catch(Exception e) {
-			System.out.println(e);
-		}
+	public Professor() {
+		super();
 	}
 }
 
@@ -176,73 +272,63 @@ public class Main {
 	public static void main(String[] args) {
 		
 		int choice=100;
-		Scanner scan=new Scanner(System.in);
-		University univ=new University();
-		
-		// 로그인 화면
-		boolean result; //로그인 성공 결과를 저장해줄 변수
-		
-		do {
-			System.out.println("==========================================");
-			System.out.println("로그인 화면");
-			System.out.println("==========================================");
+		Person per = new Person();
 
-			System.out.println("학번 : ");
-			int num=scan.nextInt();
-			scan.nextLine();	//개행문자 제거 
-			System.out.println("메일 : ");
-			String mail=scan.nextLine();
-			
-			univ.setNumber(num);
-			univ.setMail(mail);
-			result=univ.loginStudent(num, mail);
-			
-		}while(result==false);
+		boolean result; //로그인 성공 결과를 저장해줄 변수
+		int loginWho;
 		
-		// 메인 메뉴 리스트
 		do {
+			loginWho = per.startMenu();
 			
-			univ.showMain();	// 메인 메뉴 리스트 출력 
-			choice=scan.nextInt();	// 사용자가 몇번을 수행할지 선택함 
-			
-			switch(choice) {
-			//전체 수업 목록 보기
-			case 1:	
-				univ.printClassList();
-				break;
-			
-			//로그인한 학생의 수강 리스트 출력하기 
-			case 2:
-				univ.printStudentClass();
-				break;
+			if(loginWho==1) {
+				Student stu=new Student();
+				stu.loginStudent();
 				
-			//로그인한 학생이 과목 수강하기 
-			case 3:
-				System.out.println("수강하고 싶은 과목 번호를 입력하시오."); scan.nextLine();	
-				int subject=scan.nextInt();
-				univ.registerClass(subject);
-				break;
-			
-			// 선택한 과목 수강 취소하기 
-			case 4: 
-				System.out.println("수강 취소하고 싶은 과목 이름을 입력하시오"); scan.nextLine();
-				String subject1=scan.nextLine();
-				univ.cancelClass(subject1);
-				break;
-			
-			// 교수 이름을 받아서 해당 교수가 강의하는 과목을 보여준다 
-			case 5:
-				System.out.println("교수 담당 과목 검색"); scan.nextLine();
-				String profName=scan.nextLine();
-				System.out.println(profName);
-				univ.professorClass(profName);
-				break;
+				// 메인 메뉴 리스트
+				do {
+					stu.showMain();	// 메인 메뉴 리스트 출력 
+					choice=stu.getChoice(); // 어떤 메뉴를 수행할지 결정
+					
+					switch(choice) {
+					//전체 수업 목록 보기
+					case 1:	
+						stu.printClassList();
+						break;
+					
+					//로그인한 학생의 수강 리스트 출력하기 
+					case 2:
+						stu.printStudentClass();
+						break;
+						
+					//로그인한 학생이 과목 수강하기 
+					case 3:
+						stu.registerClass();
+						break;
+					
+					// 선택한 과목 수강 취소하기 
+					case 4:
+						stu.cancelClass();
+						break;
+					
+					// 교수 이름을 받아서 해당 교수가 강의하는 과목을 보여준다 
+					case 5:
+						stu.professorClass();
+						break;
+					}
+					
+				}while(choice!=0);
+				
+				stu.closeAll();
+				
+			} 
+			// 교수로 로그인 했을 때 
+			else if (loginWho==2){
+				
 			}
 			
-		}while(choice!=0);
+		}while((loginWho!=1)&&(loginWho!=2));
 		
-		univ.closeDB();
-		scan.close();
+		
 	}//main문 괄호 닫음
 	
 }// Test 클래스 괄호 닫음
